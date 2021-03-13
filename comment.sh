72d75071c6486151f951877577e41bd03dc438ba
Manjunadh Kandavalli <49728844+manju838@users.noreply.github.com>
2021-03-13 21:29:32 +0530

    Add files via upload

---
+++ comment.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#Make sure to append bash by a "/" in path orelse it gives bad interpreter error message
+
+echo "Single line comment is by prefixing with a hash sign"
+#Single line comment
+
+echo "Multi line comment is by the syntax : 'Multiline comment' "
+: '
+This
+is 
+a 
+Multiline
+comment'
