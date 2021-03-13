3dff5c6a1c841fb048711bbc7cb1385b98647845
Manjunadh Kandavalli <49728844+manju838@users.noreply.github.com>
2021-03-13 21:40:04 +0530

    Add files via upload

---
+++ hereDocDelimiter.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#hereDocDelimiter is any variable
+#To print output on terminal we use <<
+
+cat << creativ 
+This is the text
+that I assigned to variable "creativ"
+creativ
+
+#Please check the spacings orelse delimiters may not get closed
